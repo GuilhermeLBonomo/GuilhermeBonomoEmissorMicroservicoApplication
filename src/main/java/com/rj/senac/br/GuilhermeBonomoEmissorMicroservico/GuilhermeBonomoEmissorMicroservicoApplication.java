f4ea19e3b7d45306e069c8c59d05d2463ab53cc7
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2024-11-28 21:08:15 -0300

    jenkins e docker

---
+++ src/main/java/com/rj/senac/br/GuilhermeBonomoEmissorMicroservico/GuilhermeBonomoEmissorMicroservicoApplication.java
@@ -0,0 +1,13 @@
+package com.rj.senac.br.GuilhermeBonomoEmissorMicroservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilhermeBonomoEmissorMicroservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuilhermeBonomoEmissorMicroservicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rj/senac/br/GuilhermeBonomoEmissorMicroservico/GuilhermeBonomoEmissorMicroservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rj.senac.br.GuilhermeBonomoEmissorMicroservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilhermeBonomoEmissorMicroservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
